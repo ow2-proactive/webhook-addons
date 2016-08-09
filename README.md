ee6fa55c634751dc4d194dcedc8f3e5d3aa81df3
Michael Benguigui <michael.benguigui@activeeon.com>
2016-08-09 09:35:01 +0200

    Project reorganization

---
+++ README.md
@@ -0,0 +1,18 @@
+This project provide an utility class but also a Java task that allow to send
+HTTP requests from a ProActive Workflow.
+
+# Installation
+
+Run the following Gradle command to generate JAR file:
+
+```
+gradle clean jar
+```
+
+It will generate a JAR file in `build/libs/webhook-addons-X.Y.Z-SNAPSHOT.jar`
+
+Copy the JAR file in the `addons` folder of your ProActive installation.
+
+# Usage
+
+A template is provided in the studio.
--- src/main/java/org/ow2/proactive/addons/webhook/task/WebhookTask.java
+++ src/main/java/org/ow2/proactive/addons/webhook/task/WebhookTask.java
@@ -1,4 +1,38 @@
-package manualtasks;
+/*
+ *  *
+ * ProActive Parallel Suite(TM): The Java(TM) library for
+ *    Parallel, Distributed, Multi-Core Computing for
+ *    Enterprise Grids & Clouds
+ *
+ * Copyright (C) 1997-2015 INRIA/University of
+ *                 Nice-Sophia Antipolis/ActiveEon
+ * Contact: proactive@ow2.org or contact@activeeon.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; version 3 of
+ * the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * If needed, contact us to obtain a release under GPL Version 2 or 3
+ * or a different license than the AGPL.
+ *
+ *  Initial developer(s):               The ProActive Team
+ *                        http://proactive.inria.fr/team_members.htm
+ *  Contributor(s):
+ *
+ *  * $$ACTIVEEON_INITIAL_DEV$$
+ */
+package org.ow2.proactive.addons.webhook.task;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -12,8 +46,7 @@ import java.util.Map;
 import org.ow2.proactive.scheduler.common.task.TaskResult;
 import org.ow2.proactive.scheduler.common.task.executable.JavaExecutable;
 
-
-public class WebHookTask extends JavaExecutable {
+public class WebhookTask extends JavaExecutable {
 
     private static final String ARG_USER_AGENT = "User-Agent";
     private static final String ARG_METHOD = "method";
