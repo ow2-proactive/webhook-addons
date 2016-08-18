b47bdca55226498d8fa85d6b9cf9fa353fca6e3f
Tobias Wiens <tobwiens@gmail.com>
2016-08-18 12:47:23 +0200

    Improve Webhook
    - Add tests
    - Change structure to be testable
    - Use libraries
    - Remove not necessary  libraries

---
+++ src/test/java/org/ow2/proactive/addons/webhook/service/JsonRestRequestServiceTest.java
@@ -0,0 +1,14 @@
+package org.ow2.proactive.addons.webhook.service;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+public class JsonRestRequestServiceTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
